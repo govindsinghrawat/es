6c02ab7e4bb52d18d60a0e149c4cc59747b67496
govindsinghrawat <govindrawat54@gmail.com>
2025-01-03 00:13:55 -0800

    Update average_shards_per_node.py

--- scripts/python/ops/average_shards_per_node.py
+++ scripts/python/ops/average_shards_per_node.py
@@ -4,7 +4,11 @@ import requests
 es_url = "http://your-elasticsearch-url:9200"
 
 # The API endpoint for `_cat/shards` with JSON format
-endpoint = f"{es_url}/_cat/shards?format=json"
+endpoint = f"{es_url}/_cat/shards?format=json&bytes=b"
+
+nodes = {
+    "data_0_0" : {"size_sum" : 1234, "shards": [ "index": index, "shard"}
+}
 
 try:
     # Make the HTTP GET request
@@ -17,6 +21,20 @@ try:
     # [ { "index": "index", "shard" : "0", 
     #     "pripre" : "p/r", "state" : "STARTED". "docs": "2", "store" : "20.9kb", 
     #     "ip" : "10.114.233.200", "node" : "data_0_0" }, {}, {} ...]
+
+    
+    for shard in shards:
+        # How many shards are on a given node
+        if shard["state"] == "STARTED":
+            # add shard count to node
+            if shard["node"] in nodes.keys():
+                nodes[shard["node"]]+=1
+            else:
+                nodes[shard["node"]]=1
+
+            #sum of size for a given node
+            
+        
     
     
 except requests.exceptions.RequestException as e:
